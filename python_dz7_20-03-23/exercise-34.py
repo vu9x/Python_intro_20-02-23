afcdfb6156ae483cf16b64f7667abeccec5408e4
Vu Nguyen <nguyentuanvu9x@gmail.com>
2023-03-25 18:08:21 +0300

    Finished: exercise-34

---
+++ python_dz7_20-03-23/exercise-34.py
@@ -0,0 +1,32 @@
+# def vowel_counter(phraze):
+#     vowels = "уеыаоэяию"
+#     counter = 0
+#     for i in phraze:
+#         if i in vowels:
+#             counter += 1
+#     return counter
+
+# def phraze_spliter(phraze):
+#     return phraze.split()
+
+# poem = "пара-ра-рам рам-пам-папам па-ра-па-да"
+# phraze_1 = phraze_spliter(poem)
+# print(phraze_1)
+
+# for i in phraze_1:
+#     print(vowel_counter(i))
+
+vowels = "уеыаоэяию"
+user_poem = "пара-ра-рам рам-пам-папам па-ра-па-да"
+result = set()
+
+phraze = lambda poem: poem.split()
+use_phraze = phraze(user_poem)
+
+for i in use_phraze:
+    result.add(len(list(filter(lambda x: x in vowels, i))))
+
+if len(result) == 1:
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
